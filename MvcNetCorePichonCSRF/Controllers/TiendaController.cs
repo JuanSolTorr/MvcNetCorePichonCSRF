e5bbfaa0e36ad74aa1d840f226b7fa6266523ca4
JuanTajamar <juan.solis@tajamar365.com>
2026-02-20 13:16:22 +0100

    Add project files.

---
+++ MvcNetCorePichonCSRF/Controllers/TiendaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcNetCorePichonCSRF.Controllers
+{
+    public class TiendaController : Controller
+    {
+        public IActionResult Productos()
+        {
+            // Si el usuario no esta validado todavía, lo llevamos a denegado
+            if(HttpContext.Session.GetString("USUARIO") == null)
+            {
+                return RedirectToAction("Denied", "Managed");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Productos(string direccion, string[] producto)
+        {
+
+        }
+    }
+}
